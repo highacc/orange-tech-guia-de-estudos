abe8003ff860c488f0dca843b7815f6773e11eb4
Olavo e Souto <olavo_souto@hotmail.com>
2022-11-17 20:10:40 -0300

    Variaveis e operadores

---
+++ Primeiras-paginas-interativas-javascript/Primeiros-passos-js/index.js
@@ -0,0 +1,51 @@
+// Declaração de variáveis e operadores
+//operador de atribuição = 
+// let variavel = 10;
+
+// console.log("Variavel = " + variavel);
+
+// let variavel2 = 10;
+
+// console.log("Variavel2 = " + variavel2);
+
+// let operacao
+
+//operador de soma +
+// soma = variavel + variavel2
+// console.log("Soma = " + soma);
+
+//operador de subtração -
+// subtracao = variavel - variavel2
+// console.log("Subtração = " + subtracao);
+
+//operador de multiplicação *
+// multiplicacao = variavel * variavel2
+// console.log("Multiplicação = " + multiplicacao);
+
+//operador de divisão /
+// divisao = variavel / variavel2
+// console.log("Divisão = " + divisao);
+
+
+/* Desafio 1 
+Faça um programa para calcular o valor gasto de combustivel em uma viagem
+
+Você terá 3 variáveis. Sendo elas:
+
+1- Preço do combustível;
+2- Valor medio de gasto do carro;
+3- Distancia em KM da viagem;
+
+Imprima no console o valor que será gasto de combustível para realizar esta viagem.*/
+
+/*const valorCombustivel = 4.89; // R$/lt
+const mediaDeConsumoVeiculo = 10; // km/lt
+const distanciaPercorrida = 100; // km 
+
+let custo = ( distanciaPercorrida / mediaDeConsumoVeiculo ) * valorCombustivel;
+
+console.log("R$"+custo);*/
+
+
+// Estruturas Condicionais
+
