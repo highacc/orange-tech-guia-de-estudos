a6998bf7ef7972ff521b2f4607ba60b615cbce82
Olavo e Souto <olavo_souto@hotmail.com>
2022-11-18 14:06:23 -0300

    Condicionais

--- Primeiras-paginas-interativas-javascript/Primeiros-passos-js/index.js
+++ Primeiras-paginas-interativas-javascript/Primeiros-passos-js/index.js
@@ -44,8 +44,50 @@ const distanciaPercorrida = 100; // km
 
 let custo = ( distanciaPercorrida / mediaDeConsumoVeiculo ) * valorCombustivel;
 
-console.log("R$"+custo);*/
+console.log("R$"+custo.toFixed(2));*/
 
 
-// Estruturas Condicionais
+// #Estruturas Condicionais
 
+// ##Booleans true e false
+
+/*const numero = 10
+
+const numeroPar = (numero % 2) === 0; //se o resto da divisão da variável numero for igual a 0 o resutado é true para numero par.
+if (numero === 0) {
+    console.log("O número é inválido");
+} else if (numeroPar) {
+    console.log("Número Par");
+} else {
+    console.log("Numero ímpar");
+}
+*/
+
+/* Desafio 2 
+Faça um programa para calcular o valor gasto de combustivel em uma viagem
+
+Você terá 5 variáveis. Sendo elas:
+
+1- Preço do etanol;
+1- Preço da gasolina;
+3- O tipo de combustível que está no seu carro;
+2- Valor medio de gasto do carro;
+3- Distancia em KM da viagem;
+
+Imprima no console o valor que será gasto de combustível para realizar esta viagem.*/
+
+/*const etanol = 3.79;
+const gasolina = 4.89;
+const tipo = 'etanol';
+const kmPorLitro = 10;
+const distanciaPercorrida = 100;
+let custo
+
+if (tipo === 'Gasolina') {
+     custo = ( distanciaPercorrida / kmPorLitro ) * gasolina;
+     console.log("R$"+custo.toFixed(2));
+}   else {
+    custo = ( distanciaPercorrida / kmPorLitro ) * etanol;
+    console.log("R$"+custo.toFixed(2));
+}
+*/
